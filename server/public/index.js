92a3f0d2fed4bd9752a91b4c75085674e1803673
ne_Sachirou <utakata.c4se@gmail.com>
2015-05-24 15:05:05 +0900

    Coding rule.

--- server/public/index.js
+++ server/public/index.js
@@ -1,19 +1,20 @@
 function Ui() {
   var me = this;
-  this.prompt = document.querySelector('.prompt');
-  this.input  = document.querySelector('.input');
-  this.ws     = new WebSocket('ws://localhost:3000/');
+  this.prompt    = document.querySelector('.prompt');
+  this.input     = document.querySelector('.input');
+  this.inputText = this.input.querySelector('.input_text');
+  this.ws        = new WebSocket('ws://localhost:3000/');
   this.input.addEventListener('submit', function (evt) {
     evt.preventDefault();
-    var text = me.input.text.value;
-    me.input.text.value = '';
+    var text = me.inputText.value;
+    me.inputText.value = '';
     me.ws.send(text);
-    me.prompt.innerHTML = '<p class="you">' + text + '</p>' + me.prompt.innerHTML;
+    me.prompt.innerHTML = '<p class="prompt_you">' + text + '</p>' + me.prompt.innerHTML;
   });
   this.ws.onopen = function (evt) {
   };
   this.ws.onmessage = function (evt) {
-    me.prompt.innerHTML = '<p class="me">' + evt.data + '</p>' + me.prompt.innerHTML;
+    me.prompt.innerHTML = '<p class="prompt_me">' + evt.data + '</p>' + me.prompt.innerHTML;
   };
   this.ws.onclose = function (evt) {
   };
